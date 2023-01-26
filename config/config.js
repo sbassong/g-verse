7a0a505e8e541ace8c1311b714e10bf2c6f468e4
Sam Bassong <sam.bassong@gmail.com>
2023-01-25 17:06:00 -0800

    to deploy on vercel

--- config/config.js
+++ config/config.js
@@ -1,22 +1,20 @@
 require('dotenv').config()
 
-module.exports = {
-  development: {
-    database: 'gameStart_development',
-    dialect: 'postgres'
-  },
-  test: {
-    database: 'gameStart_test',
-    dialect: 'postgres'
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-        require: true
-      }
+export const development = {
+  database: 'gameStart_development',
+  dialect: 'postgres'
+}
+export const test = {
+  database: 'gameStart_test',
+  dialect: 'postgres'
+}
+export const production = {
+  use_env_variable: 'DATABASE_URL',
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+      require: true
     }
   }
 }
