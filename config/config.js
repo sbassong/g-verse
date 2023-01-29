f33b89057f5c16163468cf3fd820f5c7eeae809a
Sam Bassong <sam.bassong@gmail.com>
2023-01-28 22:35:48 -0800

    commit to deploy

--- config/config.js
+++ config/config.js
@@ -4,12 +4,10 @@ module.exports = {
   development: {
     database: 'gameStart_development',
     dialect: 'postgres',
-    host: "127.0.0.1",
   },
   test: {
     database: 'gameStart_test',
     dialect: 'postgres',
-    host: "127.0.0.1",
   },
   production: {
     use_env_variable: 'DATABASE_URL',
