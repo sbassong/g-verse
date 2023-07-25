88a8af78aa9f87ce432ce3d791c32dc39569201a
Sam Bassong <sam.bassong@gmail.com>
2023-07-25 15:44:28 -0700

    secret key fix attempt 1

--- middleware/index.js
+++ middleware/index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 const APP_SECRET = process.env.APP_SECRET
+console.log(APP_SECRET)
 
 const hashPassword = async (password) => {
   let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
