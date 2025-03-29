6a1412d034a852637f6352dc1234082a235798c1
Sam Bassong <sam.bassong@gmail.com>
2025-03-29 15:19:54 -0700

    saving after  downgrading node to 22

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use('/api', AppRouter);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   })
 }
 
