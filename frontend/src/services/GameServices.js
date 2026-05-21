73eddcab060aec07112a33d82d0e7b9f2eaad067
Sam Bassong <sam.bassong@gmail.com>
2026-05-21 12:36:20 -0700

    testing services

--- frontend/src/services/GameServices.js
+++ frontend/src/services/GameServices.js
@@ -3,6 +3,7 @@ import Client from './api'
 export const GetGames = async () => {
   try {
     const res = await Client.get('/games')
+    console.log({res})
     return res.data
   } catch (error) {
     throw error
